926f31534a6d03935bb6e55a9b63ebaef0b1be87
haun <haun@126.com>
2021-01-24 15:45:54 +0800

    update deletetask

--- gen_apk.sh
+++ gen_apk.sh
@@ -6,17 +6,18 @@
 
 rm app/libs/hello*
 rm app/apks/*
-rm hellolib/out_aar/*
 
-#gradle hellolib:assemble  # build hellolib ;这个也ok,但是gradle是系统的程序
+#gradle hellolib:assemble  # 使用安装的gradle也行,gradlew是对gradle的封装
 ./gradlew hellolib:makeaar  # 只编译 hellolib,不编译app模块
 cp hellolib/out_aar/*.aar app/libs/ #hellolib编译的aar拷贝至app使用
 
 #./gradlew app:clean assembleRelease #build app
-./gradlew app:clean assemble #build app
+#./gradlew app:clean assemble #build app
+./gradlew app:clean makeapk #build app
 adb shell pm uninstall com.jiage.gradledemo
 sleep 0.5
-adb install ./app/build/outputs/apk/debug/app-debug.apk
+#adb install ./app/build/outputs/apk/debug/app-debug.apk
+adb install ./app/apks/*.apk
 #adb install ./app/build/outputs/apk/release/app-release-unsigned.apk
 adb shell am start -n com.jiage.gradledemo/.MainActivity
 
